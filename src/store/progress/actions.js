1283e54c132bce872af838ef63b540b96b7d2005
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-10-19 18:44:38 +0900

    feat(store/progress): add progress handler store

---
+++ src/store/progress/actions.js
@@ -0,0 +1,32 @@
+import { CHANGE, PROGRESS, SUCCEED, FAIL, DONE } from './mutation-types'
+
+const WAIT_TIME = 700
+// const TIMEOUT_TIME = 15000
+
+function timeout(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+export default {
+  [PROGRESS]({ commit }) {
+    commit(CHANGE, PROGRESS)
+  },
+
+  async [SUCCEED]({ commit, dispatch }) {
+    commit(CHANGE, SUCCEED)
+    await timeout(WAIT_TIME)
+    dispatch(DONE)
+  },
+
+  async [FAIL]({ commit, dispatch }) {
+    commit(CHANGE, FAIL)
+    await timeout(WAIT_TIME)
+    dispatch(DONE)
+  },
+
+  [DONE]({ commit }) {
+    commit(CHANGE, null)
+  }
+}
